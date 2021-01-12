13f0464bf1736ae095be2dd253d304180b5e72ee
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2021-01-12 17:26:07 +0000

    tidied code and files, imported bootstrap via terminal

--- src/App.js
+++ src/App.js
@@ -7,25 +7,23 @@ import Forecast from "./Forecast";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="weather-app">
-          <Search />
-          <Header />
+      <div className="weather-app">
+        <Search />
+        <Header />
 
-          <br />
-          <Forecast />
-          <script src="src/index.js"></script>
-        </div>
-        <span className="open-source-link">
-          <a
-            href="https://github.com/Kathrin-ddggxh/weather-react"
-            target="_blank"
-          >
-            Open-source code{" "}
-          </a>
-          by Kathrin Welfare
-        </span>
+        <br />
+        <Forecast />
+        <script src="src/index.js"></script>
       </div>
+      <span className="open-source-link">
+        <a
+          href="https://github.com/Kathrin-ddggxh/weather-react"
+          target="_blank"
+        >
+          Open-source code{" "}
+        </a>
+        by Kathrin Welfare
+      </span>
     </div>
   );
 }
